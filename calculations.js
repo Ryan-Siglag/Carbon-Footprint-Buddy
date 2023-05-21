2d29485859ab588d1ab85b9318ce4bd3a5bb2507
Ryan-Siglag <Ryan.Siglag@gmail.com>
2023-05-21 16:10:41 -0400

    Final

--- calculations.js
+++ calculations.js
@@ -2,9 +2,6 @@
 var multipliers = [105, 102, 113, .79, 1100, 4400, 184, 166]
 var dataValues = document.getElementById("data");
 
-sessionStorage.setItem('total', 88);
-
-
 dataValues.addEventListener("submit", (e) => {
     e.preventDefault();
     var values = dataValues.elements;
@@ -13,10 +10,6 @@ dataValues.addEventListener("submit", (e) => {
     for (var i = 0; i < values.length-3; i++){
         var currVal = values[i].value;
         total += currVal * multipliers[i];
-        
-        // if (currVal != null){
-        //     total += currVal * multipliers[i];
-        // }
     }
 
     for (var i = values.length-3; i < values.length-1; i++){
@@ -29,10 +22,4 @@ dataValues.addEventListener("submit", (e) => {
     alert(total);
     sessionStorage.setItem('total', total);
     window.location.href = "/results.html"
-    // var finalText = document.getElementById("total");
-    // finalText.innerHTML = "Total: " + total;
-
-    
-    //alert("Yee haw")
-
 });
